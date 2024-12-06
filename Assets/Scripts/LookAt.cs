71efce10234dbfc69c4f0fd75569584c090c699a
isaacMullen <43121643+isaacMullen@users.noreply.github.com>
2024-12-06 09:49:20 -0400

    I give up on the ball detection.
    Nothing is working properly.

--- Assets/Scripts/LookAt.cs
+++ Assets/Scripts/LookAt.cs
@@ -1,235 +1,142 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
-using UnityEngine.UIElements;
-using Unity.VisualScripting;
 
-public class LookAt : MonoBehaviour
-{        
+public class EnemyAI : MonoBehaviour
+{
     public Animator animator;
     private Quaternion targetRotation;
-    
-    public Transform player;    
-    Vector3 directionToPlayer;
-    Vector3 lastRecordedPlayerPosition;
 
-    public TextMeshProUGUI dotProductText;
-    public TextMeshProUGUI isInViewText;
+    public Transform player; // Reference to the player
+    public List<Transform> balls; // List of balls or other targets
 
-    //Materials to switch between when in view
-    public Material nonAggroMaterial;
-    public Material aggroMaterial;
-   
-    Vector3 edgeDirection;
-    Vector3 negativeEdgeDirection;
-            
     public float moveSpeed;
-    public float rotationSpeed;   
+    public float rotationSpeed;
 
     public float viewAngle = 60f;
     public float aggroRange;
-    
-    bool enemyAggrod;
-    bool shouldChasePlayer;
-    bool goToLastPosition;
-    
-    bool visionCheckOnCooldown;
-    bool visionCheckRunning;
-    bool checkingVision;
 
-    float dotProduct;
+    public LayerMask detectionLayers;
+    public LayerMask obstacleLayers;
 
+    private Transform currentTarget;
+    private Vector3 lastKnownPosition;
+    private bool isChasingLastPosition;
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawLine(transform.position, lastRecordedPlayerPosition);
-        
-        //Drawing a line to the player we are looking at
-        Gizmos.color = Color.blue;
-        Gizmos.DrawLine(transform.position, player.position);
-        //Getting the direction from the enemy (looking object) to the player (object to look at)
-        Vector3 directionToObject = (player.position - transform.position).normalized;
-
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform.position, transform.forward * 2);
-
-        //Determining the dot product
-        dotProduct = Vector3.Dot(transform.forward, directionToObject);
-
-        dotProductText.text = $"Dot Product: {dotProduct}";             
-
-        //Getting the directions to the edges of the viewpoint
-        edgeDirection = Quaternion.Euler(0, (viewAngle / 2), 0) * transform.forward;
-        negativeEdgeDirection = Quaternion.Euler(0, (-viewAngle / 2), 0) * transform.forward;
-
-        //Drawing the lines on the edge of the viewing frustum
-        Gizmos.color = Color.green;
-        Gizmos.DrawLine(transform.position, transform.position + edgeDirection * 10);
-        Gizmos.DrawLine(transform.position, transform.position + negativeEdgeDirection * 10);
-
-        //Comparing the dot product to the angle of view on each side of the forward direction. Hence dividing the viewangle by 2.
-        //If i didn't divide it by 2, the 45 degree angle would be used on each side, resulting in a 90 degree total viewing angle.
-        if (dotProduct > Mathf.Cos((viewAngle * .5f) * Mathf.Deg2Rad) && Vector3.Distance(transform.position, player.position) < aggroRange)
-        {
-            isInViewText.text = $"In View";
-            GetComponent<Renderer>().material = aggroMaterial;
-        }
-        
-        else
-        {
-            isInViewText.text = $"Out of View";
-            GetComponent<Renderer>().material = nonAggroMaterial;
-        }
-    }
-    // Start is called before the first frame update
-            
-    // Update is called once per frame
     void Update()
     {
-        EnemyAggro(); //RETURNING A FLAG TO CALL THE CHASE METHOD IN FIXED UPDATE       
+        DetectTargets();
     }
-   
+
     private void FixedUpdate()
     {
-        //Debug.Log(shouldChasePlayer);
-        if (enemyAggrod)
-        {
-            StartCoroutine(DirectVisionCheck());            
-        } 
-        if(shouldChasePlayer || goToLastPosition)
-        {
-            ChasePlayer();
-        }
-        
-    }
-    bool EnemyAggro()
-    {                                        
-        //GETTING DIFFERENCE BETWEEN PLAYER POSITION AND CURRENT POSITION (NORMALIZED)
-        directionToPlayer = (player.position - transform.position).normalized;
-
-        //Determining the dot product
-        dotProduct = Vector3.Dot(transform.forward, directionToPlayer);
-
-        dotProductText.text = $"Dot Product: {dotProduct}";
-
-        //IF THE PLAYER IS INSIDE THE VIEWING ANGLE AND IN RANGE
-        if (dotProduct > Mathf.Cos((viewAngle * .5f) * Mathf.Deg2Rad) && Vector3.Distance(transform.position, player.position) < aggroRange)
-        {
-            
-            isInViewText.text = $"In View";            
-            GetComponent<Renderer>().material = aggroMaterial;
-            return enemyAggrod = true;
-        }
-        else
-        {
-            isInViewText.text = $"Out of View";            
-            GetComponent<Renderer>().material = nonAggroMaterial;
-            return enemyAggrod = false;
-            
-        }        
+        MoveToTarget();
     }
 
-    //CHECKS FOR THE SHADER
-    
-
-    void ChasePlayer()
+    void DetectTargets()
     {
-        var step = rotationSpeed * Time.deltaTime;
-                
-        if (shouldChasePlayer)
+        Transform bestTarget = null;
+        float closestDistance = Mathf.Infinity;
+
+        // Check if the player is visible
+        if (CanSeeTarget(player))
         {
-            //SETTING THE TARGET ROTATION TO ROTATE TOWARDS USING THE NORMALIZED DIFFERENCE OF PLAYER AND CURRENT POSITION 
-            directionToPlayer = (player.position - transform.position).normalized;
-            targetRotation = Quaternion.LookRotation(directionToPlayer);
-            
-            //MOVE TOWARDS PLAYER
-            transform.position = Vector3.MoveTowards(transform.position, player.position, moveSpeed);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, step);
-
-
-        }
-        else if(goToLastPosition)
-        {
-            //SETTING THE TARGET DIRECTION TO ROTATE TOWARDS BASED ON THE NOMRALIZED DIFFERENE OF LAST RECORDED AND CURRENT POSITION
-            Vector3 directionToLastKnownPosition = (lastRecordedPlayerPosition - transform.position).normalized;
-            targetRotation = Quaternion.LookRotation(directionToLastKnownPosition);
-            
-            //MOVE TOWARDS PLAYERS LAST POSITION
-            transform.position = Vector3.MoveTowards(transform.position, lastRecordedPlayerPosition, moveSpeed);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, step);
-            //Debug.Log("Going To Last Location");
-            
-
-            if (transform.position == lastRecordedPlayerPosition)
+            float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+            if (distanceToPlayer < closestDistance)
             {
-                //PLAYING THE ANIMATION WHEN THE ENEMY REACHES PLAYERS LAST KNOW LOCATION
-                animator.enabled = true;
-
-                //STOPPING THE MOVEMENT ONCE THE ENEMY GETS TO THE PLAYERS LAST RECORD POSITION. LATER HE WILL LOOK AROUND FOR THEM.
-                goToLastPosition = false;
-                shouldChasePlayer = false;
+                closestDistance = distanceToPlayer;
+                bestTarget = player;
             }
         }
-        //ENEMY WILL CONTINUE TO RAYCAST AS LONG AS THE PLAYER IS DETECTED. OTHERWISE ENEMY JUST CONTINUES TO MOVE TO HIS LAST POSITION ('checkingVision' IS TOGGLED IN THE RAYCAST COROUTINE)       
-        if(checkingVision)
+
+        // Check if any ball is visible
+        foreach (Transform ball in balls)
         {
-            StartCoroutine(DirectVisionCheck());
-        }                                                         
+            if (CanSeeTarget(ball))
+            {
+                float distanceToBall = Vector3.Distance(transform.position, ball.position);
+                if (distanceToBall < closestDistance)
+                {
+                    closestDistance = distanceToBall;
+                    bestTarget = ball;
+                }
+            }
+        }
+
+        // Update the current target
+        if (bestTarget != null)
+        {
+            currentTarget = bestTarget;
+            lastKnownPosition = bestTarget.position;
+            isChasingLastPosition = false;
+        }
+        else if (currentTarget != null)
+        {
+            // If we lose sight of the target, move to the last known position
+            currentTarget = null;
+            isChasingLastPosition = true;
+        }
     }
 
-    IEnumerator DirectVisionCheck()
+    bool CanSeeTarget(Transform target)
     {
-        //IF THE COROUTINE ISNT ALREADY RUNNING
-        if(visionCheckRunning) yield break;
-        
-        //SET THE COROUTINE FLAG TO RUNNING
-        visionCheckRunning = true;
-        
-        //SETTING A TOGGLE, INSTEAD OF A TYPICAL RETURN VALIE OF TRUE OR FALSE, SO THAT THE ENEMY WILL CONTINUE TO RAYCAST ANYTIME IT IS MOVING
-        //AS WELL AS ANYTIME IT IS AGGROD BY SEEING THE PLAYER ('ENEMYAGGRO' STARTS THIS COROUTINE)
-        checkingVision = true;
+        Vector3 directionToTarget = (target.position - transform.position).normalized;
+        float dotProduct = Vector3.Dot(transform.forward, directionToTarget);
 
-        //COOLDOWN FOR THE RAYCAST
-        yield return new WaitForSeconds(.01f);
-        
-        //SELECTING MASKS TO INTERACT WITH
-        LayerMask layerMask = LayerMask.GetMask("Walls", "Player");
-        string objectHit = null;
-
-        //SENDING OUT A RAY TOWARDS THE PLAYER, RETURNING THE LAYERMASK THAT IT DETECTS, EITHER 'PLAYER' OR 'WALLS'
-        if (Physics.Raycast(transform.position, directionToPlayer, out RaycastHit hit, Mathf.Infinity, layerMask))
+        // Check if the target is within view angle
+        if (dotProduct > Mathf.Cos((viewAngle * 0.5f) * Mathf.Deg2Rad))
         {
-            //CHECKING WHICH OBJECT WAS HIT
-            objectHit = hit.collider.gameObject.name;
-            Debug.Log($"Object Hit By Cast: {objectHit}");
-            
-            //Debug.Log($"Recorded Position: {lastRecordedPlayerPosition} | {objectHit}");
+            // Perform a raycast to check for obstacles
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, directionToTarget, out hit, aggroRange, detectionLayers | obstacleLayers))
+            {
+                Debug.Log("Raycast hit: " + hit.transform.name);
+                if (hit.transform == target)
+                {
+                    return true; // Target is visible
+                }
+            }
+            else
+            {
+                Debug.Log("Raycast did not hit anything.");
+            }
         }
-        
-        if(objectHit == "Player")
-        {
-            //STOPPING ANIMATION WHEN THE ENEMY CAN SEE THE PLAYER
-            animator.enabled = false;
+        return false; // Target is not visible
+    }
 
-            //STORING PLAYERS POSITION ON RAYCASTHIT
-            lastRecordedPlayerPosition = player.position;
-            
-            //START B-LINING PLAYER
-            goToLastPosition = false;
-            shouldChasePlayer = true;            
+    void MoveToTarget()
+    {
+        Vector3 targetPosition;
+
+        if (currentTarget != null)
+        {
+            // Move towards the current target
+            targetPosition = currentTarget.position;
+        }
+        else if (isChasingLastPosition)
+        {
+            // Move towards the last known position
+            targetPosition = lastKnownPosition;
+
+            // Stop chasing if we've reached the last known position
+            if (Vector3.Distance(transform.position, lastKnownPosition) < 0.5f)
+            {
+                isChasingLastPosition = false;
+                return;
+            }
         }
         else
         {
-            //STOP RAYCASTING TO THE PLAYERONCE THE HE IS HIDDEN.
-            checkingVision = false;
-            
-            //START B-LINING PLAYERS LAST SEEN POSITION
-            shouldChasePlayer = false;
-            goToLastPosition = true;            
+            // No target and not chasing last position
+            return;
         }
-                
-        //RESETTING THE COROUTINE FLAG SO IT CAN BE RUN AGAIN.
-        visionCheckRunning = false;               
+
+        Vector3 directionToTarget = (targetPosition - transform.position).normalized;
+        targetRotation = Quaternion.LookRotation(directionToTarget);
+
+        // Rotate toward the target
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        // Move toward the target
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
     }
 }
--- Assets/Scripts/ProjectileHandler.cs
+++ Assets/Scripts/ProjectileHandler.cs
@@ -1,11 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class ProjectileHandler : MonoBehaviour
 {
-    public LayerMask layerMask;   
+    public LayerMask layerMask;
+
+    public static ProjectileHandler Instance;
+    public Vector3 currentPosition { get; private set; }
 
     public float shotSpeed;
     bool shotStopped;
@@ -17,7 +17,7 @@ public class ProjectileHandler : MonoBehaviour
 
     private void Start()
     {
-        
+        Instance = this;
     }
 
     private void OnEnable()
@@ -25,22 +25,25 @@ public class ProjectileHandler : MonoBehaviour
         // Reset timer and initialize direction
         timer = 0f;
         velocity = transform.forward * shotSpeed;
+        shotStopped = false;
     }
 
     // Update is called once per frame     
     private void Update()
     {
-        if(!shotStopped)
+        currentPosition = transform.position;
+
+        if (!shotStopped)
         {
             MoveProjectile();
         }
-                               
+
         // Increment timer and disable object if its lifetime expires
         timer += Time.deltaTime;
         if (timer > shotTime)
         {
             gameObject.SetActive(false);
-        }        
+        }
     }
 
     void MoveProjectile()
@@ -48,22 +51,23 @@ public class ProjectileHandler : MonoBehaviour
         Ray ray = new Ray(transform.position, velocity);
         RaycastHit hit;
 
-        if(Physics.Raycast(ray, out hit, shotSpeed * Time.deltaTime, layerMask))
+        if (Physics.Raycast(ray, out hit, shotSpeed * Time.deltaTime, layerMask))
         {
             velocity = Vector3.Reflect(velocity, hit.normal);
 
             transform.position = hit.point + velocity.normalized * 0.1f;
         }
         else
-        {           
+        {
             transform.position += velocity * Time.deltaTime;
 
-            velocity *= dampingFactor; 
-            if (velocity.magnitude <= 0.1f)  
+            velocity *= dampingFactor;
+            if (velocity.magnitude <= 0.1f)
             {
                 shotStopped = true;
             }
         }
+        currentPosition = transform.position;
     }
-    
+
 }
